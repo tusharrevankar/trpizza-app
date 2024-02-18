3fe1cb3dbe125387fc3dd366b29b64e01b63279d
tusharrevankar <tusharrevankar@gmail.com>
2024-02-18 14:55:44 +0530

    fix

--- components/Add.jsx
+++ components/Add.jsx
@@ -44,7 +44,7 @@ const Add = ({ setClose }) => {
         img: url,
       };
 
-      await axios.post("http://localhost:3000/api/products", newProduct);
+      await axios.post("http://trpizzawebapp/api/products", newProduct);
       setClose(true);
     } catch (err) {
       console.log(err);
